706020682108a9bddfb0a812459aea20dae6478d
avnish986 <avnishgupta986@gmail.com>
2020-10-10 15:34:33 +0530

    [Avnish]Added: ability to pop and peek in a stack

--- src/test/java/com/data/structures/StackTest.java
+++ src/test/java/com/data/structures/StackTest.java
@@ -19,4 +19,19 @@ public class StackTest {
 		stack.print();
 
 	}
+
+	@Test
+	public void given3NumberWhenPopStackShouldBeLastAddedNode() {
+		Node<Integer> firstNode = new Node<Integer>(70);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(56);
+		MyStack stack = new MyStack();
+		stack.push(firstNode);
+		stack.push(secondNode);
+		stack.push(thirdNode);
+		INode pop = stack.pop();
+		Assert.assertEquals(thirdNode, pop);
+		stack.print();
+
+	}
 }
