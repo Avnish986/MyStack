198760a9a8f23b8f49f16becbf0d1ccdde5cbff7
avnish986 <avnishgupta986@gmail.com>
2020-10-13 15:48:27 +0530

    [Avnish]Added:hash map using hash function and hash table REMOVE

--- src/test/java/com/data/structures/MyLinkedHashMapTest.java
+++ src/test/java/com/data/structures/MyLinkedHashMapTest.java
@@ -22,4 +22,26 @@ public class MyLinkedHashMapTest {
 		System.out.println(map);
 		Assert.assertEquals(3, freq);
 	}
+	@Test
+	public void givenASentence_WhenWordsAreAddedToList_ShouldRemoveAvoidable() {
+		String sen = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately into " + "paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> map = new MyLinkedHashMap<>();
+		String[] words = sen.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer val = (Integer) map.get(word);
+			if (val == null)
+				val = 1;
+			else
+				val += 1;
+			map.add(word, val);
+		}
+		System.out.println(map);
+		map.remove("avoidable");
+		System.out.println("");
+		System.out.println(map);
+		int freq = 0;
+		if(map.get("avoidable") == null) freq = -1;
+		Assert.assertEquals(-1, freq);
+	}
 }
